f7a369a84c29d4069ea1ae317c2076419f79b503
Shivubha vaghela <shivubha24@gmail.com>
2026-07-07 02:56:49 +0530

    Create Type.kt

---
+++ app/src/main/java/com/crushshiv/app/ui/theme/app/src/main/java/com/crushshiv/app/ui/theme/Type.kt
@@ -0,0 +1,23 @@
+package com.crushshiv.app.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+val CrushShivTypography = Typography(
+
+    titleLarge = TextStyle(
+        fontSize = 28.sp,
+        fontWeight = FontWeight.Bold
+    ),
+
+    titleMedium = TextStyle(
+        fontSize = 22.sp,
+        fontWeight = FontWeight.SemiBold
+    ),
+
+    bodyLarge = TextStyle(
+        fontSize = 16.sp
+    )
+)
