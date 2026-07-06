802e061290d4c9ec13f0ec4ba929dcce6654e6b7
Shivubha vaghela <shivubha24@gmail.com>
2026-07-07 02:56:05 +0530

    Create Color.kt

---
+++ app/src/main/java/com/crushshiv/app/ui/theme/Color.kt
@@ -0,0 +1,14 @@
+package com.crushshiv.app.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val PurpleCandy = Color(0xFF9C27B0)
+val PinkCandy = Color(0xFFE91E63)
+val BlueCandy = Color(0xFF2196F3)
+val GreenCandy = Color(0xFF4CAF50)
+val YellowCandy = Color(0xFFFFEB3B)
+val OrangeCandy = Color(0xFFFF9800)
+
+val GoldPremium = Color(0xFFFFD700)
+val DarkBackground = Color(0xFF121212)
+val WhiteCandy = Color(0xFFFFFFFF)
