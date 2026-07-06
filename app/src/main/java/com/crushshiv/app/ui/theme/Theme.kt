4e1231f7716e8986b8fdb5c28d7cdb8f5f8b4936
Shivubha vaghela <shivubha24@gmail.com>
2026-07-07 02:55:09 +0530

    Create Theme.kt

---
+++ app/src/main/java/com/crushshiv/app/ui/theme/Theme.kt
@@ -0,0 +1,13 @@
+package com.crushshiv.app.ui.theme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+
+@Composable
+fun CrushShivTheme(
+    content: @Composable () -> Unit
+) {
+    MaterialTheme(
+        content = content
+    )
+}
