555cf560da0cdea0e7ee786f4f734049beeed24c
Shivubha vaghela <shivubha24@gmail.com>
2026-07-07 02:12:19 +0530

    Update MainActivity.kt

--- MainActivity.kt
+++ MainActivity.kt
@@ -20,7 +20,9 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CrushShivApp()
+
+    CrushShivNavigation()
+            ()
         }
     }
 }
